5e8faa6e07892a33b0a0f2516187096e2d4d541f
Revanth Gowda Vimala Kempegowda <vkrevanthgowda@gmail.com>
2023-03-14 15:45:26 -0500

    Final

---
+++ app.js
@@ -0,0 +1,21 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+let navToggle = document.querySelector(".nav-toggle");
+let links = document.querySelector(".links");
+
+navToggle.addEventListener("click",function(){
+    //console.log(links.classList);
+    // console.log(links.classList.contains("random"))
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }
+    // else{
+    //     links.classList.add('show-links')
+    // }
+    links.classList.toggle("show-links");
+
+});
